eff4710c0b47f71a6669482a671548093a4c53a3
Beccatoni <rbccmtni1@gmail.com>
2024-10-03 11:34:56 +0200

    chore:  crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StudentSchema } from './schema/student.schema';
+import { StudentService } from './student/student.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, StudentService],
 })
 export class AppModule {}
